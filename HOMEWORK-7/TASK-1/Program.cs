03a051520b38a9b6b59fdd79bbc587ccdf066e4e
Vlad <vlad.melnik@bk.ru>
2022-07-19 19:59:18 +0300

    1task added

---
+++ HOMEWORK-7/TASK-1/Program.cs
@@ -0,0 +1,27 @@
+﻿double[,] CreateRandom2dArray(int rows, int columns)
+{
+	double[,] newArray = new double[rows, columns];
+
+	for(int i = 0; i < rows; i++)
+		for(int j = 0; j < rows; j++)
+			newArray[i,j] = new Random().NextDouble()*10;
+	return newArray;
+}
+void Show2dArray(double[,] array)
+{
+	for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+			Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] myArray = CreateRandom2dArray(m,n);
+Show2dArray(myArray);
+
